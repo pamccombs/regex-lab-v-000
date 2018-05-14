10ce1485ce49af2f1ecf9f7d22e7fbea7d953a87
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-14 16:43:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,23 @@
 def starts_with_a_vowel?(word)
-
+#if 
+word.match(/\A[aeiou]\w+/i)? true : false
+  #true
+#else
+ # false
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+text.scan(/un\w+ing/i)
 
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match()
 end
 
 def valid_phone_number?(phone)
